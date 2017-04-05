b36291d7da2eaa3286719768c5bc1552d04d3461
Leandro Martinez <leandromartinez98@gmail.com>
2017-04-05 10:34:41 -0300

    remvoed unecessary data from resetboxes

--- resetboxes.f90
+++ resetboxes.f90
@@ -15,7 +15,7 @@
 subroutine resetboxes()
       
   use sizes
-  use compute_data, only : latomfirst, latomfix, nboxes, &
+  use compute_data, only : latomfirst, latomfix, &
                            lboxfirst, lboxnext, hasfree
   implicit none
   integer :: i, j, k, ibox
@@ -23,38 +23,9 @@ subroutine resetboxes()
 
   ! Reset boxes
 
-  latomfirst = 0
+  latomfirst = 0 ! Reset all boxes - three dimensional array
 
-  !do i = 1, nboxes(1)
-  !  do j = 1, nboxes(2)
-  !    do k = 1, nboxes(3)
-  !      latomfirst(i,j,k) = latomfix(i,j,k)
-  !    end do
-  !  end do
-  !end do
-
-  !! Reset margins
-  !    
-  !do j = 0, nboxes(2)+1
-  !  do k = 0, nboxes(3)+1
-  !    latomfirst(0,j,k) = 0
-  !    latomfirst(nboxes(1)+1,j,k) = 0
-  !  end do
-  !end do
-
-  !do i = 0, nboxes(1)+1
-  !  do k = 0, nboxes(3)+1
-  !    latomfirst(i,0,k) = 0
-  !    latomfirst(i,nboxes(2)+1,k) = 0
-  !  end do
-  !end do
-
-  !do i = 0, nboxes(1)+1
-  !  do j = 0, nboxes(2)+1
-  !    latomfirst(i,j,0) = 0
-  !    latomfirst(i,j,nboxes(3)+1) = 0
-  !  end do
-  !end do   
+  ! Reset data for boxes that contain fixed atom
 
   ibox = lboxfirst
   do while( ibox > 0 ) 
