f012d261655519a647888e77930625cd91f8234b
Leandro Martinez <leandromartinez98@gmail.com>
2017-04-06 19:55:50 -0300

    Changed version file to 17.096

--- title.f90
+++ title.f90
@@ -18,7 +18,7 @@ subroutine title()
   write(*,"(' PACKMOL - Packing optimization for the automated generation of', /&
            &' starting configurations for molecular dynamics simulations.', /&
            &' ',/&
-           &t62,' Version 17.094 ')")
+           &t62,' Version 17.096 ')")
   write(*,hash3_line)
 
 end subroutine title
