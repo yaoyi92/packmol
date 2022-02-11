5d41198954748dea86334865a8111eb031b4ef2f
Leandro Martinez <leandro@iqm.unicamp.br>
2022-02-10 23:18:53 -0300

    Changed version file to 20.3.4

--- title.f90
+++ title.f90
@@ -13,7 +13,7 @@ subroutine title()
   write(*,"(' PACKMOL - Packing optimization for the automated generation of', /&
            &' starting configurations for molecular dynamics simulations.', /&
            &' ',/&
-           &t62,' Version 20.3.3 ')")
+           &t62,' Version 20.3.4 ')")
   write(*,hash3_line)
 
 end subroutine title
