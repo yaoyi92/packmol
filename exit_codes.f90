f15c9134c9d72c776c7f553cdd59d6ab53a54b43
Sasha Fonari <fonari@schrodinger.com>
2022-11-01 15:14:03 -0400

    Add a comment about exit codes values

--- exit_codes.f90
+++ exit_codes.f90
@@ -8,6 +8,7 @@ module exit_codes
 
   IMPLICIT NONE
 
+  ! Codes 1, 2, 126 – 165 and 255 have special meaning
   integer, parameter :: exit_code_success = 0
   integer, parameter :: exit_code_general_error = 170
   integer, parameter :: exit_code_input_error = 171
