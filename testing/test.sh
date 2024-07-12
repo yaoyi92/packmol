064ecb2fcd99aaece6b404fa3e7f7d7aefc24e14
Leandro Martinez <lmartine@unicamp.br>
2024-07-12 10:19:29 -0300

    add bilayer test

--- testing/test.sh
+++ testing/test.sh
@@ -6,7 +6,8 @@ curl -fsSL https://install.julialang.org | sh
 julia runtests.jl ./input_files/water_box.inp \
                   ./input_files/ieee_signaling.inp \
                   ./input_files/mixture.inp \
-                  ./input_files/spherical.inp
+                  ./input_files/spherical.inp \
+                  ./input_files/bilayer.inp
 
 # check if output files are properly generated in a failed run
 ./test_failed.sh ./input_files/water_box_failed.inp packmol.log "FORCED" 
