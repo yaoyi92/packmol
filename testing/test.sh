048a4b53025f3bdebb8ca467c9b98d54a94058f1
Leandro Martinez <lmartine@unicamp.br>
2024-07-12 09:52:27 -0300

    restore testing of generation of failed packing files

--- testing/test.sh
+++ testing/test.sh
@@ -4,4 +4,7 @@
 curl -fsSL https://install.julialang.org | sh
 # Run the tests
 julia runtests.jl water_box.inp \
-                  ieee_signaling.inp
+                  ieee_signaling.inp 
+
+# check if output files are properly generated in a failed run
+./test_failed.sh water_box_failed.inp packmol.log "FORCED" 
