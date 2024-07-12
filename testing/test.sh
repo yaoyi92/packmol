f4470cb173814af991b1d2d88ea0ac669395d5e5
Yi Yao <yaoyi92@gmail.com>
2024-07-12 14:52:28 +0000

    move the pbc example to the input_files folder

--- testing/test.sh
+++ testing/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
 # Install Julia
-curl -fsSL https://install.julialang.org | sh
+#curl -fsSL https://install.julialang.org | sh
 # Run the tests
 julia runtests.jl ./input_files/water_box.inp \
                   ./input_files/ieee_signaling.inp \
                   ./input_files/mixture.inp \
                   ./input_files/spherical.inp \
                   ./input_files/bilayer.inp \
-                  ./input_files/solvprotein.inp
+                  ./input_files/solvprotein.inp \
+		  ./input_files/water_box_pbc.inp
 
 # check if output files are properly generated in a failed run
 ./test_failed.sh ./input_files/water_box_failed.inp packmol.log "FORCED" 
